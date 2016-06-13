0624a733fb336205eaaa86541ed8ba06a7cbf00b
Damien VAN ROBAEYS <damien.vanrobaeys@gmail.com>
2016-06-13 16:13:41 +0200

    Add files via upload

---
+++ Game_Of_Thrones_Theme.ps1
@@ -0,0 +1,34 @@
+#========================================================================
+#
+# Tool Name	: Game Of Thrones Theme 
+# Author 	: Damien VAN ROBAEYS
+# Date 		: 13/06/2016
+# Website	: http://www.systanddeploy.com/
+# Twitter	: https://twitter.com/syst_and_deploy
+#
+#========================================================================
+
+[console]::beep(700,1000)       
+[console]::beep(450,1000)     
+[console]::beep(550,150)     
+[console]::beep(620,150)     
+[console]::beep(700,850)       
+[console]::beep(450,850)     
+[console]::beep(550,150)     
+[console]::beep(620,150)     
+[console]::beep(520,1000)  
+    sleep -Milliseconds 200   
+[console]::beep(620,1000)     
+[console]::beep(420,800)     
+[console]::beep(520,150)     
+[console]::beep(550,150)     
+[console]::beep(620,1000)     
+[console]::beep(420,800)     
+[console]::beep(550,150)     
+[console]::beep(520,150)     
+[console]::beep(450,800)     
+
+
+# [console]::beep(520,1000) 
+    # sleep -Milliseconds 200
+# [console]::beep(620,1000) 
